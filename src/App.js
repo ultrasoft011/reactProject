e119c5d4fbd9ebbbf823bc55a63e6001099923aa
ultrasoft011 <ultrasoft.011@gmail.com>
2021-05-01 17:17:29 -0500

    style JSX

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React from 'react';
 
 function App() {
   return <div>
-    <Folder name="Music"/>
-    <Folder name="Type"/>
+    <Folder name="Emotions">
+      <File name="Happy Chill"/>
+      <File name="Melancolic"/>
+    </Folder>
+    <Folder name="Groups">
+      <File name="Beats"/>
+    </Folder>
   </div>
 }
 
 const Folder = (props) => {
-  return <h4>{props.name}</h4>
+  const borderStyle = { border: '2px solid pink' };
+  return <div style = {borderStyle}> 
+    <h1>{props.name}</h1>
+    {props.children}
+  </div>
+};
+
+const File = (props) => {
+  return <div>
+    {props.name}
+  </div>
 }
 
 export default App;
